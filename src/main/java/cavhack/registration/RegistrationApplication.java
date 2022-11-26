465f7887e843c579718914928e626aa8b83a42a9
jt914 <39843887+jt914@users.noreply.github.com>
2022-11-26 10:06:25 -0500

    make it better

--- src/main/java/cavhack/registration/RegistrationApplication.java
+++ src/main/java/cavhack/registration/RegistrationApplication.java
@@ -2,8 +2,16 @@ package cavhack.registration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@ComponentScan({"cavhack.registration.participant"})
+@EntityScan("cavhack.registration")
+@EnableJpaRepositories("cavhack.registration.repository")
 public class RegistrationApplication {
 
 	public static void main(String[] args) {
