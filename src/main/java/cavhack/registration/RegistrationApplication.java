135e97aeaab939f5c3a3fa223db54f76fc80a034
jt914 <39843887+jt914@users.noreply.github.com>
2022-11-26 10:13:04 -0500

    wasd

--- src/main/java/cavhack/registration/RegistrationApplication.java
+++ src/main/java/cavhack/registration/RegistrationApplication.java
@@ -2,16 +2,8 @@ package cavhack.registration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-@ComponentScan({"cavhack.registration.participant"})
-@EntityScan("cavhack.registration")
-@EnableJpaRepositories("cavhack.registration.repository")
+@SpringBootApplication
 public class RegistrationApplication {
 
 	public static void main(String[] args) {
